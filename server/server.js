99de6dc1fc1d2f6e06ce88b7966520103c7f9bc6
Sihayah <sihaya_a@hotmail.com>
2022-02-24 02:21:58 -0500

    created react app with homepage rendering thoughtList and added concurrently

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const { ApolloServer } = require('apollo-server-express');
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
+const { authMiddleware } = require('./utils/auth');
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -11,7 +13,7 @@ const startServer = async() => {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    // context: authMiddleware
+    context: authMiddleware
   });
 
   await server.start();
@@ -25,6 +27,15 @@ startServer();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
