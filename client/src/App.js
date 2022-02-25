03c2b3fe95c7be5b2423d5af305d647ea9754dc0
Sihayah <sihaya_a@hotmail.com>
2022-02-24 20:12:59 -0500

    .

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,12 @@ import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@ap
 import Header from './components/Header';
 import Footer from './components/Footer';
 
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
 import Home from './pages/Home';
 
 const httpLink = createHttpLink({
@@ -18,15 +24,24 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className='flex-column justify-flex-start min-100-vh'>
-        <Header />
-        <div className='container'>
-          <Home />
+      <Router> 
+        <div className='flex-column justify-flex-start min-100-vh'>
+          <Header />  
+          <div className='container'>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/profile/:username" component={Profile} />
+            <Route exact path="/thought/:id" component={SingleThought} />
+
+            <Route component={NoMatch} />
+          </Switch>            
         </div>
         <Footer />
-      </div>      
+        </div>  
+      </Router>    
     </ApolloProvider>
-
   );
 }
 
